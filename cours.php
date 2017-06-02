a8a96cdfb158361e70fc2b2e367ea43de38fd059
vcoquinot <vcoquinot@gmail.com>
2017-06-02 11:52:04 +0200

    Create cours.php

---
+++ cours.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Nataraj Bollywood cours</title>
+  <meta charset="utf-8"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <link href="https://fonts.googleapis.com/css?family=Kalam" rel="stylesheet">
+  <link rel="stylesheet" media="screen and (max-width: 1280px)" href="petite_resolution.css" />
+</head>
+
+<body>
+<!--MENU-->
+<nav class="navbar navbar-default navbar-fixed-top" id="navbar">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="index.php">Anne Nataraj</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav pull-right">
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="anne.php">Anne</a></li>
+        <li><a href="bollywood.php">Bollywood</a></li>
+        <li class="active"><a href="cours.php">Cours & stages</a></li>
+        <li><a href="agenda.php">Agenda</a></li>
+        <li><a href="galerie.php">Galerie</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="membres.php">Membres</a></li>
+      </ul>
+        
+        
+    </div>
+  </div>
+</nav>
+
+<!-- INTITULE COURS-->
+	<section id="cours">
+        <div class="row text-center">
+       	<h4><strong>Le centre Nataraj Bollywood vous propose :</strong></h4>
+		</div>
+    <section class="container">
+    		<div class="row">
+    			<figure class="col-xs-12 col-md-2 col-lg-3">
+    			<img id ="pic4" src="photo_anne_4.jpg" alt="Anne" />
+    			</figure>
+      			<figure class="col-md-6 col-lg-6">
+					<p><strong> Des cours de danse Bollywood</strong></p>
+				</figure>
+				<figure class="col-md-6 col-lg-6">
+					<p><strong>Du Fitness Bollywood</strong><br/>Alliance du Fitness et des mouvements de la Danse Bollywood, ou comment garder la forme avec le sourire !<br/> Vous allez adorer !</p>
+				</figure>
+				<figure class="col-md-6 col-lg-6">
+					<p><strong>Des ateliers Posture-Assouplissement Pré-Maman</strong><br/>Des exercices en souplesse et en douceur pour faire vivre aux futures mamans une grossesse harmonieuse !</p>
+				</figure>
+				<figure class="col-md-6 col-lg-6">
+				<p><strong>Des ateliers Parent-Bébé</strong><br/>Un moment de complicité et d'éveil avec Bébé, dès 10 mois !<br/>Une parenthèse de bonheur !</p> 
+				</figure>
+				<figure class="col-md-3 col-lg-3">
+                <img id=""lotus" src="photo_flower.png" alt="lotus" />
+                </figure>
+            </div>
+            <div id="essai">
+                <div row="row-text-center">
+                <p><strong><em>Un premier cours d'essai vous est offert, alors n'hésitez plus :<br/> venez rejoindre le Centre de Danse Nataraj pour vous laisser emporter dans un tourbillon de mélodies indiennes en dansant sur les musiques des plus grands succès des films Bollywood !</em></strong></p>
+                </div>
+	       </div>
+	</section>
+</section>
+
+
+<!-- PLANNING-->
+	<section id="planning">
+        <div class="row text-center">
+		<h4><strong>PLANNING DES COURS</strong></h4>
+        </div> 
+			<section class="container">
+    			<div class="row">
+                <figure class="col-xs-4 col-md-1">
+                    <img src="location_mpl.png" alt="location" />
+                </figure>
+      			   <figure class="col-xs-5 col-md-3">
+					   <h5><strong>Mardi</strong></h5>
+                <P>M Yoga<br/>11 avenue de Lodève</P>
+				    </figure>
+                <figure class="col-xs-4 col-md-1">
+                    <img src="location_mauguio.png" alt="location" />
+                </figure>
+      			   <figure class="col-xs-5 col-md-3">
+					   <h5><strong>Mercredi</strong></h5>
+					       <P>Gymnase de l'Etang de l'Or<br/>rue Alexandre Dumas</P>
+				    </figure>
+                    <figure class="col-xs-4 col-md-1">
+                    <img src="location_lunel.png" alt="location" />
+                </figure>
+                 <figure class="col-xs-5 col-md-3">
+					   <h5><strong>Jeudi</strong></h5>
+					     <P>Dynamic Dance<br/>150 rue Gustave Eiffel</P>
+				    </figure>
+				</div>
+	       </section>
+</section>
+
+
+<!-- STAGES-->
+	<section id="STAGES">
+        <div class="row text-center">
+            <h4><strong>Stages</strong></h4>
+        </div>
+    <section class="container">
+        <div class="row">
+            <figure class='col-md-4'>
+                <img src="photo_lotus.png" alt="lotus" />
+            </figure>
+             <figure class='col-md-8'>
+                <ul>
+			        <li><h5><strong>Samedi 18 mars 2017</strong></h5><em>Stage Garba-Dandiya Raas à Marseille.</em></li>
+			        <li><h5><strong>Samedi 22 avril 2017</strong></h5><em>Stage "Bollywood en duo"!</em><br/>Venez danser et partager une chorégraphie à 2 !<br/>13h30-16h30 Salle de danse du Gymnase de l'Etang de l'Or à Mauguio.<br/>30€ par personne.</li>
+			        <li><h5><strong>Samedi 17 juin 2017</strong></h5><em>Stage Parents-Enfants</em><br/> venez partager un moment de complicité avec votre enfant ! 14h-16h Salle de danse du Gymnase de l'Etang de l'Or à Mauguio. 20€ par personne.</li>
+		        </ul>
+            </figure>
+        </div>          
+    </section>
+    </section>
+    
+
+<!--FOOTER-->
+<?php include("footer.php"); ?>
+</body>
+</html>
